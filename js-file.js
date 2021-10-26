f71990e35f63553bd5a9260a7106f17e02ebab29
Torres2235 <Torres2235@gmail.com>
2021-10-26 14:28:55 -0700

    adding basic functionality to calculator

--- js-file.js
+++ js-file.js
@@ -40,19 +40,52 @@ function operate(x, y, op) {
 
 
 let input = ''; //hold our string
+let x;
+let y;
+let op = '';
+let total;
 
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => { //updates our string with the number we pressed
         if(input.length < 19) {
-            input = input + button.id;
+            input = input + button.textContent;
             document.querySelector('#display').textContent = input;
         }
         if(button.id == 'clear') {//clears our display and our "input" string
             input = '';
+            x = !x;
+            y = !y;
+            op = '';
+            total = !total;
             document.querySelector('#display').textContent = '0';
         }
         
+        if(button.id == 'add' || button.id == 'sub' || button.id == 'multiply' || button.id == 'divide') {
+            x = parseInt(input);
+            op = String(button.id);
+            //console.log(x);
+            //console.log(op);
+            input = '';
+            document.querySelector('#display').textContent = '0';
+        }
+
+        if(button.id == 'equal') {
+            if(!x || !op) {
+                //console.log("ERROR");
+                input = '';
+                document.querySelector('#display').textContent = '0';
+            } else {
+                y = parseInt(input);
+                //console.log(y);
+
+                total = operate(x, y, op);
+                document.querySelector('#display').textContent = total;
+                x = total;
+            }
+
+
+        }
     });
 });
     
